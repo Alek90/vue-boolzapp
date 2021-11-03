455f78fed5a6aba6d1f7e8dd30440a5c9b3e85bb
Alek90 <4lek.90@gmail.com>
2021-11-03 11:51:58 +0100

    Iniziato .side_bar > header.

--- milestone-1/assets/app.js
+++ milestone-1/assets/app.js
@@ -4,6 +4,17 @@ const root = new Vue({
 
     data:{
 
+        user: [
+            
+            {
+                name: 'EleZe',
+                avatar: '_io',
+                visible: true,
+            
+            }
+            
+        ],
+
         contacts: [
             {
                 name: 'Michele',
@@ -11,19 +22,19 @@ const root = new Vue({
                 visible: true,
                 messages: [
                     {
-                    date: '10/01/2020 15:30:55',
-                    text: 'Hai portato a spasso il cane?',
-                    status: 'sent'
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        status: 'sent'
                     },
                     {
-                    date: '10/01/2020 15:50:00',
-                    text: 'Ricordati di dargli da mangiare',
-                    status: 'sent'
+                        date: '10/01/2020 15:50:00',
+                        text: 'Ricordati di dargli da mangiare',
+                        status: 'sent'
                     },
                     {
-                    date: '10/01/2020 16:15:22',
-                    text: 'Tutto fatto!',
-                    status: 'received'
+                        date: '10/01/2020 16:15:22',
+                        text: 'Tutto fatto!',
+                        status: 'received'
                     }
                 ],
             },
@@ -33,19 +44,19 @@ const root = new Vue({
                 visible: true,
                 messages: [
                     {
-                    date: '20/03/2020 16:30:00',
-                    text: 'Ciao come stai?',
-                    status: 'sent'
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
                     },
                     {
-                    date: '20/03/2020 16:30:55',
-                    text: 'Bene grazie! Stasera ci vediamo?',
-                    status: 'received'
+                        date: '20/03/2020 16:30:55',
+                        text: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
                     },
                     {
-                    date: '20/03/2020 16:35:00',
-                    text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                    status: 'sent'
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
                     }
                 ],
             },
@@ -55,19 +66,19 @@ const root = new Vue({
                 visible: true,
                 messages: [
                     {
-                    date: '28/03/2020 10:10:40',
-                    text: 'La Marianna va in campagna',
-                    status: 'received'
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
                     },
                     {
-                    date: '28/03/2020 10:20:10',
-                    text: 'Sicuro di non aver sbagliato chat?',
-                    status: 'sent'
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
                     },
                     {
-                    date: '28/03/2020 16:15:22',
-                    text: 'Ah scusa!',
-                    status: 'received'
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
                     }
                 ],
             },
@@ -77,19 +88,19 @@ const root = new Vue({
                 visible: true,
                 messages: [
                     {
-                    date: '10/01/2020 15:30:55',
-                    text: 'Lo sai che ha aperto una nuova pizzeria?',
-                    status: 'sent'
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
                     },
                     {
-                    date: '10/01/2020 15:50:00',
-                    text: 'Si, ma preferirei andare al cinema',
-                    status: 'received'
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
                     }
                 ],
             },
         ],
-        
+
     }
 })
 
